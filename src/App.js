e1a08440170b8a6aa6d6f3470f9dd5c3ff68a850
Vanessa <leungkachiiiii@gmail.com>
2022-01-20 00:51:07 -0500

    event binding

--- src/App.js
+++ src/App.js
@@ -11,12 +11,17 @@ class App extends Component {
       monsters: [],
       searchField: ""
     };
+
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
     .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value});
   }
 
   render() {
@@ -28,7 +33,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div> 
